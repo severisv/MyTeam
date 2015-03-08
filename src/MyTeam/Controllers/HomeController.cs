be25e4026096a79793a9bc68bcc3f0ccdba5c0c9
fiskeboss <severisv@gmail.com>
2015-03-08 16:50:55 +0100

    First commit

---
+++ src/MyTeam/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace MyTeam.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.Message = "Test";
+            return View();
+        }
+     
+        
+    }
+}  
