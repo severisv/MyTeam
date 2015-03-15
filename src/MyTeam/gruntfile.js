37d623e9e916a109788947b87625ae278c22cbd4
fiskeboss <severisv@gmail.com>
2015-03-15 20:55:46 +0100

    added less and watch

--- src/MyTeam/gruntfile.js
+++ src/MyTeam/gruntfile.js
@@ -1,4 +1,5 @@
-﻿// This file in the main entry point for defining grunt tasks and using grunt plugins.
+/// <binding AfterBuild='less:development' ProjectOpened='bower:install, watch, watch:less' />
+// This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 
 module.exports = function (grunt) {
@@ -11,6 +12,23 @@ module.exports = function (grunt) {
                     cleanTargetDir: false
                 }
             }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["Assets"]
+                },
+                files: { "wwwroot/css/site.css": "assets/site.less" }
+            }
+        },
+        watch: {
+            less: {
+                files: ["assets/*.less"],
+                tasks: ["less"],
+                options: {
+                    livereload: true
+                }
+            }
         }
     });
 
@@ -20,4 +38,6 @@ module.exports = function (grunt) {
     // The following line loads the grunt plugins.
     // This line needs to be at the end of this this file.
     grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 };
