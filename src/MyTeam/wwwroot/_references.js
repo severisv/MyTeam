e1c8474c20945d8a6354f916b42de341f7cd915b
fiskeboss <severisv@gmail.com>
2015-04-06 11:25:56 +0200

    wwwroot

--- src/MyTeam/wwwroot/_references.js
+++ src/MyTeam/wwwroot/_references.js
@@ -4,3 +4,4 @@
 /// <reference path="lib/bootstrap/js/bootstrap.js" />
 /// <reference path="lib/jquery/js/jquery.js" />
 /// <reference path="lib/bootstrap/js/npm.js" />
+/// <reference path="lib/jquery-ajax-unobtrusive/jquery.unobtrusive-ajax.min.js" />
