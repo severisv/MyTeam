3cc340e55155f2a7ae4bb839fd4ba1e9a20e63e9
fiskeboss <severisv@gmail.com>
2015-04-06 11:33:46 +0200

    viewmodels

--- src/MyTeam/ViewModels/Player/ShowPlayersViewModel.cs
+++ src/MyTeam/ViewModels/Player/ShowPlayersViewModel.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyTeam.ViewModels.Player
 {
     public class ShowPlayersViewModel
     {
+   
+        public IEnumerable<Models.Domain.Player> Players { get; set; }
+
+        public ShowPlayersViewModel(IQueryable<Models.Domain.Player> players)
+        {
+            Players = players;
+        }
 
     }
 }
