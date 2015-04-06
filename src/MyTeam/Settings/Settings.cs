d837d3e30bed258206e8f94b3c21e2f268f22fb2
fiskeboss <severisv@gmail.com>
2015-04-06 11:33:32 +0200

    set

--- src/MyTeam/Settings/Settings.cs
+++ src/MyTeam/Settings/Settings.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using MyTeam.Models.Enums;
 
 namespace MyTeam.Settings
 {
-    public class Settings
+    public class Config
     {
         public static string Title => ClubName;
         public static string ClubName = "Wam-Kam";
+
+     
+
+        public static string PlayerImages(string clubShortname, string imagename, ImageSize imageSize)
+        {
+            if (string.IsNullOrWhiteSpace(imagename)) return string.Format("~/img/default_player.gif");
+
+            Regex rgx = new Regex("[^a-zA-Z0-9]");
+            var handle = rgx.Replace(clubShortname, "").ToLower();
+            var size = GetSize(imageSize);
+            return string.Format("~/img/clubs/{0}/players/{1}_{2}.jpg", handle, imagename, size);
+        }
+
+        private static string GetSize(ImageSize imageSize)
+        {
+            switch (imageSize)
+            {
+                case ImageSize.Small:
+                    return "sm";
+                case ImageSize.Medium:
+                    return "md";
+                case ImageSize.Full:
+                    return "full";
+                default:
+                    throw new NotImplementedException(string.Format("Imagesize not recognized: {0}", imageSize));
+            }
+        }
+
     }
 }
