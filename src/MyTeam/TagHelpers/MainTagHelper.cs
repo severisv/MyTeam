a03a01813b8415e1ebc8f607ecdbe47100a54b92
fiskeboss <severisv@gmail.com>
2015-04-06 11:33:38 +0200

    taghelpers

---
+++ src/MyTeam/TagHelpers/MainTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.TagHelpers;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace MyTeam.TagHelpers
+{
+    [TagName("mt-main")]
+    [ContentBehavior(ContentBehavior.Modify)]
+    public class MainTagHelper : TagHelper
+    {
+
+       
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.AddCssClass("col-lg-9 col-md-9");
+            var innertag = new TagBuilder("div");
+            innertag.AddCssClass("mt-container");
+            innertag.InnerHtml = output.Content;
+
+            string innerId;
+            output.Attributes.TryGetValue("inner-id", out innerId);
+            innertag.Attributes["id"] = innerId;
+
+            tagBuilder.InnerHtml = innertag.ToString();
+
+            output.MergeAttributes(tagBuilder);
+            output.Content = tagBuilder.InnerHtml;
+
+            output.TagName = "div";
+
+
+        }
+
+    }
+}
---
+++ src/MyTeam/TagHelpers/SidebarTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.TagHelpers;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace MyTeam.TagHelpers
+{
+    [TagName("mt-sidebar")]
+    [ContentBehavior(ContentBehavior.Modify)]
+    public class SidebarTagHelper : TagHelper
+    {
+
+       
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var tagBuilder = new TagBuilder("div");
+            tagBuilder.AddCssClass("col-lg-3 col-md-3 visible-md visible-lg pull-right");
+            var innertag = new TagBuilder("div");
+            innertag.AddCssClass("mt-container");
+            innertag.InnerHtml = output.Content;
+
+            string innerId;
+            output.Attributes.TryGetValue("inner-id", out innerId);
+            innertag.Attributes["id"] = innerId;
+
+            tagBuilder.InnerHtml = innertag.ToString();
+
+            output.MergeAttributes(tagBuilder);
+            output.Content = tagBuilder.InnerHtml;
+
+            output.TagName = "div";
+
+
+        }
+
+    }
+}
+
+
