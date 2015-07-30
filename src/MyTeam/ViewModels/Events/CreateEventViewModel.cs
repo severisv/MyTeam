f9f6578ca06a8f247390e0320dd4e0aafbe411ba
fiskeboss <severisv@gmail.com>
2015-07-30 17:55:46 +0200

    SelectList

--- src/MyTeam/ViewModels/Events/CreateEventViewModel.cs
+++ src/MyTeam/ViewModels/Events/CreateEventViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using Microsoft.AspNet.Mvc.Rendering;
 using MyTeam.Models.Enums;
 using MyTeam.Resources;
 
@@ -14,21 +15,23 @@ namespace MyTeam.ViewModels.Events
         public EventType Type { get; set; }
         
         [Required]
-        [DisplayName(Res.Date)]
+        //[DisplayName(Res.Date)]
         public DateTime Date { get; set; }
 
         [Required]
-        [DisplayName(Res.Time)]
+        //[DisplayName(Res.Time)]
         public TimeSpan Time { get; set; }
         [Required]
-        [DisplayName(Res.Location)]
+        //[DisplayName(Res.Location)]
         public string Location { get; set; }
         public string Description { get; set; }
 
         public bool Weekly { get; set; }
         public DateTime? ToDate { get; set; }
 
-        public IEnumerable<EventType> EventTypes => Enum.GetValues(typeof(EventType)).Cast<EventType>().Where(e => e != EventType.Alle);
+        public IEnumerable<SelectListItem> EventTypes => Enum.GetValues(typeof(EventType)).Cast<EventType>().Where(e => e != EventType.Alle).Select(e =>
+         new SelectListItem { Text = e.ToString(), Value = ((int)e).ToString()}
+        );
 
         public CreateEventViewModel()
         {
