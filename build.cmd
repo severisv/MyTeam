72b738aa828c2445695eff9848153eb9d404e09e
Severin Sverdvik <severisv@gmail.com>
2016-01-31 10:21:49 +0100

    remove npm jobs

--- build.cmd
+++ build.cmd
@@ -1,9 +1,5 @@
 @echo off
 cls
 NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-NuGet.exe "Install" "Node.js" "-OutputDirectory" "packages" "-ExcludeVersion"
-SET NodeJsPath=%~dp0packages\Node.js
-SET PATH=%PATH%;%NodeJsPath%
-NuGet.exe "Install" "Npm.js" "-OutputDirectory" "packages" "-ExcludeVersion"
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+call npm install bower -g
 "packages\FAKE\tools\Fake.exe" build.fsx %*
