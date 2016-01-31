a414a7dc60f17ac8e4a69c73cc48b379618521e5
Severin Sverdvik <severisv@gmail.com>
2016-01-31 18:55:34 +0100

    installer gulp globalt i build.cmd

--- build.cmd
+++ build.cmd
@@ -2,5 +2,5 @@
 cls
 NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 call npm install bower -g
-call npm install gulp-g
+call npm install gulp -g
 "packages\FAKE\tools\Fake.exe" build.fsx %*
