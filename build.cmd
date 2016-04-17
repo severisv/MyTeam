a2ee3e4fac49904249745da565c894335b065617
Severin Sverdvik <severisv@gmail.com>
2016-04-17 18:17:24 +0200

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,5 +3,7 @@ cls
 NuGet.exe "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 call npm install bower -g
 call npm install gulp -g
-call setdeploycredentials.cmd
+
+if exist setdeploycredentials.cmd (call setdeploycredentials.cmd) 
+
 "packages\FAKE\tools\Fake.exe" build.fsx %*
