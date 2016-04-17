a8d7f77b3200c113d657c5042bbd31f96c2f3797
Severin Sverdvik <severisv@gmail.com>
2016-04-17 19:18:11 +0200

    ws

--- src/MyTeam/Controllers/ApiController.cs
+++ src/MyTeam/Controllers/ApiController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNet.Mvc;
-using Microsoft.Data.Entity;
 using MyTeam.Filters;
 using MyTeam.Models;
 using MyTeam.Models.Enums;
