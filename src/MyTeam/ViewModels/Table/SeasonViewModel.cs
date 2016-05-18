79718f371f9636e90a27ca0c09b3379b9776bc70
Severin Sverdvik <severisv@gmail.com>
2016-05-18 17:49:21 +0200

    Bare vis når tabellen ble oppdatert hvis den er for gjeldende sesong

--- src/MyTeam/ViewModels/Table/SeasonViewModel.cs
+++ src/MyTeam/ViewModels/Table/SeasonViewModel.cs
@@ -15,5 +15,6 @@ namespace MyTeam.ViewModels.Table
         public string DisplayName => StartDate.Year == EndDate.Year ? $"{Name} - {EndDate.Year}" : $"{Name} - {StartDate.Year} / {EndDate.Year}";
         public string DisplayNameShort => StartDate.Year == EndDate.Year ? $"{EndDate.Year}" : $"{StartDate.Year} / {EndDate.Year}";
         public DateTime TableUpdated { get; set; }
+        public bool? IsActive => StartDate <= DateTime.Now && DateTime.Now <= EndDate;
     }
 }
