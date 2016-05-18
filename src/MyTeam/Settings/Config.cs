daa3c12031fd9d1b6942174ada2769a5c93c51d4
Severin Sverdvik <severisv@gmail.com>
2016-05-18 22:51:23 +0200

    Øke påmeldingstid til 14 dager

--- src/MyTeam/Settings/Config.cs
+++ src/MyTeam/Settings/Config.cs
@@ -6,7 +6,7 @@ namespace MyTeam.Settings
 {
     public class Config
     {
-        public static int AllowedSignupDays = 10;
+        public static int AllowedSignupDays = 14;
         public static int AllowedSignoffHours = 2;
         public static int AllowedMonthsAheadInTimeForTrainingCreation = 11;
         
@@ -15,10 +15,12 @@ namespace MyTeam.Settings
         {
             if (string.IsNullOrWhiteSpace(imagename)) return string.Format("~/img/default_player.gif");
 
-            Regex rgx = new Regex("[^a-zA-Z0-9]");
+
+
+            var rgx = new Regex("[^a-zA-Z0-9]");
             var handle = rgx.Replace(clubShortname, "").ToLower();
             var size = GetSize(imageSize);
-            return string.Format("~/img/clubs/{0}/players/{1}_{2}.jpg", handle, imagename, size);
+            return $"~/img/clubs/{handle}/players/{imagename}_{size}.jpg";
         }
 
         private static string GetSize(ImageSize imageSize)
