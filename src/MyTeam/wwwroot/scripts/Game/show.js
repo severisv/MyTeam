56a4f9c31d6f611739b4d905dacb7d6755dc45cb
Severin Sverdvik <severisv@gmail.com>
2016-05-22 11:36:38 +0200

    ikke prøv å react-rendre kamphendelser for kamper som ikke har passert

--- src/MyTeam/wwwroot/scripts/Game/show.js
+++ src/MyTeam/wwwroot/scripts/Game/show.js
@@ -1,20 +1,22 @@
 ﻿
 var element = $('#game-showEvents');
 
+if (element.length) {
+    var registerEvents = React.createElement(window.ShowGame, {
+        routes: {
+            GET_SQUAD: element.data('get-squad-url'),
+            GET_EVENTTYPES: element.data('get-gameeventtypes-url'),
+            ADD_EVENT: element.data('add-gameevent-url'),
+            GET_EVENTS: element.data('get-gameevents-url'),
+            DELETE_EVENT: element.data('delete-event-url'),
+            SHOW_PLAYER: element.data('show-player-url')
+        },
+        editMode: false,
+        gameId: element.data('gameid')
 
-var registerEvents = React.createElement(window.ShowGame, {
-    routes: {
-        GET_SQUAD: element.data('get-squad-url'),
-        GET_EVENTTYPES: element.data('get-gameeventtypes-url'),
-        ADD_EVENT: element.data('add-gameevent-url'),
-        GET_EVENTS: element.data('get-gameevents-url'),
-        DELETE_EVENT: element.data('delete-event-url'),
-        SHOW_PLAYER: element.data('show-player-url')
-    },
-    editMode: false,
-    gameId: element.data('gameid')
-
-});
+    });
 
 
-ReactDOM.render(registerEvents, document.getElementById("game-showEvents"));
+    ReactDOM.render(registerEvents, document.getElementById("game-showEvents"));
+}
+
