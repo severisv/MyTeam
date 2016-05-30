609a7f2d57d2f04f5401d9fb568164f4f9075c62
Severin Sverdvik <severisv@gmail.com>
2016-05-30 19:17:21 +0200

    ajax-update post på blur closes #140

--- src/MyTeam/Assets/Scripts/ajaxHelpers.js
+++ src/MyTeam/Assets/Scripts/ajaxHelpers.js
@@ -3,14 +3,20 @@
 
 ajax.applyFormUpdateListener = function ($scope) {
     var typingTimer;
-    $scope.find('.ajax-update').on('input', function () {
-
+    var updateElements = $scope.find('.ajax-update');
+    updateElements.on('input', function () {
         var element = $(this);
         var value = element.val();
         clearTimeout(typingTimer);
         typingTimer = setTimeout(function () {
             $.post(element.data('href'), { value: value });
         }, 500);
+    });
+    updateElements.on('blur', function () {
+        var element = $(this);
+        var value = element.val();
+        clearTimeout(typingTimer);
+        $.post(element.data('href'), { value: value });
     });
 }
 
