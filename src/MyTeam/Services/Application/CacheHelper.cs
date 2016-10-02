25e7813ffd9a4a653b4e8e4c308ede3fa1df1c00
Severin Sverdvik <severisv@gmail.com>
2016-10-02 10:08:34 +0200

    Endre notification-tid for bøter

--- src/MyTeam/Services/Application/CacheHelper.cs
+++ src/MyTeam/Services/Application/CacheHelper.cs
@@ -137,7 +137,7 @@ namespace MyTeam.Services.Application
             var unansweredEventIds = ids.Where(i => !answeredEvents.Contains(i)).Distinct();
 
 
-            var fineDate = DateTime.Now.AddDays(-7);
+            var fineDate = DateTime.Now.AddDays(-90);
             var hasDueFines = _dbContext.Fines.Any(f => f.Issued <= fineDate && f.Paid == null && f.MemberId == memberId);
             var due = 0;
             if (hasDueFines)
