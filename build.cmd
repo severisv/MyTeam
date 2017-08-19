e5e097422fcd8c9b57ccb60bc92cac5106d2c8b4
Severin Sverdvik <severisv@gmail.com>
2017-08-19 13:03:14 +0200

    Installer bower igjen

--- build.cmd
+++ build.cmd
@@ -4,11 +4,11 @@ if not EXIST "packages\FAKE\tools\Fake.exe" NuGet.exe "Install" "FAKE" "-OutputD
 
 
 
-REM call npm install bower -g
-REM call npm install gulp -g
-REM call npm install npm@5 -g
+call npm install bower -g
+call npm install gulp -g
+call npm install npm@5 -g
 
-REM set PATH=%APPDATA%\npm;%PATH%
+set PATH=%APPDATA%\npm;%PATH%
 
 if exist setdeploycredentials.cmd (call setdeploycredentials.cmd)
 
