c9fa28d9bc0b77eb70e7f42a84ffe91a33fe3a08
Severin Sverdvik <severisv@gmail.com>
2018-03-04 18:57:01 +0100

    Konverter tilbake til .net core

--- dev.cmd
+++ dev.cmd
@@ -3,4 +3,5 @@ call code .
 cd server
 call npm install
 call dotnet restore
+call npm run copy-libs
 call npm run watch
--- src/services/features/game/Persistence.fs
+++
@@ -1,9 +0,0 @@
-namespace MyTeam.Games
-
-open System
-open MyTeam
-open MyTeam.Domain
-open MyTeam.Models
-open MyTeam.Models.Domain
-open MyTeam.Validation
-
