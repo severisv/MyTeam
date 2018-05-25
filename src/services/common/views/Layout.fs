849be9469c18d41c171052d1c64dae27fb485b63
Severin Sverdvik <severisv@gmail.com>
2018-05-25 16:16:11 +0200

    Bust cache

--- src/services/common/views/Layout.fs
+++ src/services/common/views/Layout.fs
@@ -46,7 +46,7 @@ module Pages =
                 o.MetaDescription.HasValue =? (meta [_name "description"; _content o.MetaDescription], emptyText)
                 title [] [encodedText <| club.Name + (o.Title.HasValue =? (" - " + o.Title, "")) ]
                 link [_rel "icon"; _type "image/png"; _href <| getImage (club.Favicon =?? club.Logo) id]
-                link [_rel "stylesheet"; _href "/compiled/site.bundle.css?v22" ]
+                link [_rel "stylesheet"; _href "/compiled/site.bundle.css?v1" ]
                 Analytics.script
             ]
             body []([
