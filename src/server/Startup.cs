6c4e0bdde8e8cd2334f9a850b0a38052c1bad19c
Severin Sverdvik <severisv@gmail.com>
2018-06-24 10:57:58 +0200

    Skru på localization

--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -92,15 +92,15 @@ namespace MyTeam
 
             var dbContext = app.ApplicationServices.GetService<ApplicationDbContext>();
             dbContext.Database.Migrate();
-        
+
             app.UseStaticFiles();
             app.UseAuthentication();
-            // app.UseRequestLocalization(new RequestLocalizationOptions
-            // {
-            //     DefaultRequestCulture = new RequestCulture("nb-NO"),
-            //     SupportedCultures = new List<CultureInfo> { new CultureInfo("nb-NO") },
-            //     SupportedUICultures = new List<CultureInfo> { new CultureInfo("nb-NO") }
-            // });
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("nb-NO"),
+                SupportedCultures = new List<CultureInfo> { new CultureInfo("nb-NO") },
+                SupportedUICultures = new List<CultureInfo> { new CultureInfo("nb-NO") }
+            });
 
             app.UseMvc(routes =>
             {
