f94cc4c49169fd7c08a619bb8923ccafd9a08fa4
Severin Sverdvik <severisv@gmail.com>
2018-11-27 08:56:51 +0100

    Simply fake installation

--- build.cmd
+++ build.cmd
@@ -3,14 +3,6 @@ cls
 
 node -v
 
-cd ..
-
-dotnet --version
-
-dotnet tool install fake-cli --tool-path .\myteam\.fake  --version 5.10.1
-
-cd myteam
-
 if exist setdeploycredentials.cmd (call setdeploycredentials.cmd)
 
-.\.fake\fake run build.fsx --parallel 3 %*
+fake run build.fsx --parallel 3 %*
