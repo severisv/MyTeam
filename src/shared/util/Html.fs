6317fdff4d9c41f8e7777cb7bbabca90ef3e5427
Severin Sverdvik <severisv@gmail.com>
2019-01-28 09:34:54 +0100

    Fix mergeClasses

--- src/shared/util/Html.fs
+++ src/shared/util/Html.fs
@@ -1,22 +1,27 @@
 module MyTeam.Shared.Util.Html
 
 open Fable.Helpers.React.Props
+open MyTeam
 
-let mergeClasses (a: IHTMLProp list) (b: IHTMLProp list) =
-    let getClass (attr: IHTMLProp list) =
+let mergeClasses (a : IHTMLProp list) (b : IHTMLProp list) =
+    let getClass (attr : IHTMLProp list) =
         attr
-        |> List.filter (fun a -> a :? HTMLAttr) 
-        |> List.map(fun a ->
-                let htmlAttr = a :?> HTMLAttr
-                match htmlAttr with
-                | ClassName c -> 
-                    c
-                | Class c -> 
-                    c
-                | _ -> ""
-        )
-
-
-    a @ b @ [Class <| String.concat " " (getClass a @ getClass b)]
-        
-   
+        |> List.filter (fun a -> a :? HTMLAttr)
+        |> List.map (fun a -> 
+               a :?> HTMLAttr
+               |> function 
+               | ClassName c -> c
+               | Class c -> c
+               | _ -> "")
+        |> String.concat " "
+        |> Strings.trim
+    
+    let excludeClasses : IHTMLProp list -> IHTMLProp list =
+        List.filter (fun e -> 
+            e :?> HTMLAttr
+            |> function
+            | ClassName c -> false
+            | Class c -> false
+            | _ -> true)
+    
+    excludeClasses a @ excludeClasses b @ [ Class(getClass a + " " + getClass b) ]
