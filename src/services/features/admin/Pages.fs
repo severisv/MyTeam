cf9c6bd01decdbce63ff5c2f0c00fb00420f7fb6
Severin Sverdvik <severisv@gmail.com>
2019-01-28 08:37:09 +0100

    Fjern gammel kode

--- src/services/features/admin/Pages.fs
+++ src/services/features/admin/Pages.fs
@@ -5,42 +5,23 @@ open MyTeam
 open MyTeam.Domain.Members
 open MyTeam.Views
 
-
 let index club user ctx =
-    [
-        mtMain [] [
-            block [] [
-                div [
-                    _id "manage-players";
-                    attr "data-statuses" (Enums.getValues<Status> () |> Json.serialize) 
-                    attr "data-roles" (Enums.getValues<Role> () |> Json.serialize)                     
-                    ] []
-            ]
-        ]
-        Admin.coachMenu               
-    ] 
+    [ mtMain [] 
+          [ block [] [ div [ _id "manage-players"
+                             attr "data-statuses" (Enums.getValues<Status>() |> Json.serialize)
+                             attr "data-roles" (Enums.getValues<Role>() |> Json.serialize) ] [] ] ]
+      Admin.coachMenu ]
     |> layout club user (fun o -> { o with Title = "Administrer spillere" }) ctx
     |> OkResult
 
-
 let invitePlayers club user ctx =
-    [    
-        div [
-            _id "add-players";
-            attr "data-statuses" (Enums.getValues<Status> () |> Json.serialize) 
-            attr "data-roles" (Enums.getValues<Role> () |> Json.serialize)                     
-            ] []
-    
-        Admin.coachMenu               
-    ] 
-    |> layout club user 
-                (fun o -> { 
-                            o with 
-                                Title = "Inviter spillere"
-                                Scripts = [ 
-                                            FacebookSdk.script ctx
-                                            Scripts.documentReady "window.mt_fb.login()" 
-                                        ]
-                        }
-                ) ctx
+    [ div [ _id "add-players"
+            attr "data-statuses" (Enums.getValues<Status>() |> Json.serialize)
+            attr "data-roles" (Enums.getValues<Role>() |> Json.serialize) ] []
+      Admin.coachMenu ]
+    |> layout club user (fun o -> 
+           { o with Title = "Inviter spillere"
+                    Scripts =
+                        [ FacebookSdk.script ctx
+                          Scripts.documentReady "window.mt_fb.login()" ] }) ctx
     |> OkResult
