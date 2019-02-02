552cf34b541bbfae09b1578e373e71e28846c865
Severin Sverdvik <severisv@gmail.com>
2019-02-02 19:02:42 +0100

    komponent

--- src/client/src/features/AddPlayers.fs
+++ src/client/src/features/AddPlayers.fs
@@ -2,15 +2,15 @@ module MyTeam.Client.Admin.AddPlayers
 
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
-open Fable.Import.React
 open MyTeam
 open MyTeam.Client.Components
 open MyTeam.Components
 open MyTeam.Shared.Components.Forms
 open MyTeam.Shared.Components.Layout
 open Shared.Features.Admin.AddPlayers
-open MyTeam.Client.Util
+open MyTeam.Client.Util.ReactHelpers
 open Thoth.Json
+
 
 let defaultForm = {
     ``E-postadresse`` = ""
@@ -46,69 +46,73 @@ let onSubmit setState () =
                               SuccessMessage = Some(sprintf "%s %s ble lagt til" state.Player.Fornavn state.Player.Etternavn)
                               Player = defaultForm })
 
-type AddPlayers(props) =
-    inherit Component<Model, State>(props)
-    do base.setInitState ({ Player = defaultForm; Message = None; Errors = []; SuccessMessage = None })
-    override this.render() =
-        let props = this.props
-        let state = this.state
-        let handleFormChange update value =
-            this.setState (fun state props -> { state with Player = update state.Player value })
+
+let handleFormChange setState update value =
+    setState (fun state props -> { state with Player = update state.Player value })
+
+        
+                
+let element props children =
+        komponent<Model, State>
+             props
+             { Player = defaultForm; Message = None; Errors = []; SuccessMessage = None }
+             None                
+             (fun (props, state, setState) ->
+                 
+                 let handleFormChange = handleFormChange setState
+                 
+                 mtMain [] [
+                     block [] [                         
+                         form [ Horizontal 3 ] [
+                             h3 [] [ str "Legg til manuelt" ]
+                             state.SuccessMessage => Alerts.success
+                             formRow [ Horizontal 2; Class "text-danger" ]
+                                     []
+                                     [ ul [] (state.Errors |> List.map (fun e -> li [] [ str e ])) ]
+         
+                             formRow [ Horizontal 2 ]
+                                     [ str "E-post" ]
+                                     [ textInput [ Placeholder "hallvardthoresen@gmail.com"
+                                                   OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with ``E-postadresse`` = value }))
+                                                   Value state.Player.``E-postadresse`` ] ]
+                             formRow [ Horizontal 2 ]
+                                     [ str "Fornavn" ]
+                                     [ textInput [ Placeholder "Hallvard"
+                                                   OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Fornavn = value }))
+                                                   Value state.Player.Fornavn ] ]
+         
+                             formRow [ Horizontal 2 ]
+                                     [ str "Mellomnavn" ]
+                                     [ textInput [ Placeholder "Jensen"
+                                                   OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Mellomnavn = value }))
+                                                   Value state.Player.Mellomnavn ] ]
+         
+                             formRow [ Horizontal 2 ]
+                                     [ str "Etternavn" ]
+                                     [ textInput [ Placeholder "Thoresen"
+                                                   OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Etternavn = value }))
+                                                   Value state.Player.Etternavn ] ]
+         
+                             formRow [ Horizontal 2 ]
+                                     []
+                                     [
+                                         SubmitButton.render
+                                             (fun o ->
+                                             { o with
+                                                   Size = Normal
+                                                   Text = "Legg til"
+                                                   SubmittedText = "Lagt til"
+                                                   Endpoint = SubmitButton.Post ("/api/members", Some state.Player)
+                                                   IsDisabled = not <| isValid state
+                                                   OnSubmit = Some <| onSubmit setState
+                                                   OnError = Some <| onError setState
+                                             })
+                                     ]
+         
+                       ]
+                     ]
+                 ]
+        )
 
 
-        mtMain [] [
-            block [] [
-                 form [ Horizontal 3 ] [
-                    h3 [] [ str "Legg til manuelt" ]
-                    state.SuccessMessage => Alerts.success 
-                    formRow [ Horizontal 2; Class "text-danger" ]
-                            []
-                            [ ul [] (state.Errors |> List.map (fun e -> li [] [ str e ])) ]
-
-                    formRow [ Horizontal 2 ]
-                            [ str "E-post" ]
-                            [ textInput [ Placeholder "hallvardthoresen@gmail.com"
-                                          OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with ``E-postadresse`` = value }))
-                                          Value state.Player.``E-postadresse`` ] ]
-                    formRow [ Horizontal 2 ]
-                            [ str "Fornavn" ]
-                            [ textInput [ Placeholder "Hallvard"
-                                          OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Fornavn = value }))
-                                          Value state.Player.Fornavn ] ]
-
-                    formRow [ Horizontal 2 ]
-                            [ str "Mellomnavn" ]
-                            [ textInput [ Placeholder "Jensen"
-                                          OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Mellomnavn = value }))
-                                          Value state.Player.Mellomnavn ] ]
-
-                    formRow [ Horizontal 2 ]
-                            [ str "Etternavn" ]
-                            [ textInput [ Placeholder "Thoresen"
-                                          OnChange (fun e -> e.Value |> (handleFormChange <| fun form value -> { form with Etternavn = value }))
-                                          Value state.Player.Etternavn ] ]
-
-                    formRow [ Horizontal 2 ]
-                            []
-                            [
-                                SubmitButton.render
-                                    (fun o ->
-                                    { o with
-                                          Size = Normal
-                                          Text = "Legg til"
-                                          SubmittedText = "Lagt til"
-                                          Endpoint = SubmitButton.Post ("/api/members", Some state.Player)
-                                          IsDisabled = not <| isValid state
-                                          OnSubmit = Some <| onSubmit this.setState
-                                          OnError = Some <| onError this.setState
-                                    })
-                            ]
-
-              ]
-            ]
-        ]
-
-
-let element = ofType<AddPlayers, _, _>
-
-ReactHelpers.render Decode.Auto.fromString<Model> clientView element 
+render Decode.Auto.fromString<Model> clientView element
--- src/client/src/util/ReactHelpers.fs
+++ src/client/src/util/ReactHelpers.fs
@@ -1,15 +1,60 @@
 module MyTeam.Client.Util.ReactHelpers
 
 open Fable.Import
+open Fable.Import.React
+open Fable.Helpers.React
 open MyTeam
 
-let render deserializeFn id comp = 
+
+let render deserializeFn id comp =
     Browser.document.getElementById id
     |> fun node ->
-        
+
         if not <| isNull node then
             node.getAttribute (Interop.modelAttributeName)
             |> deserializeFn
             |> function
              | Ok model -> ReactDom.render (comp model [], node)
              | Error e -> failwithf "Json deserialization failed: %O" e
+
+
+type Lifecycles = {
+    ComponentDidMount : unit -> unit }
+
+type OutProps<'Props, 'State> =
+    'Props * 'State * (('State -> 'Props -> 'State) -> unit)
+
+type UpdateFn<'TState> = 'TState -> 'TState
+
+type ComponentProps<'Props, 'State> = {
+    Props : 'Props
+    InitialState : 'State
+    Lifecycles : Lifecycles option
+    Children : OutProps<'Props, 'State> -> ReactElement
+ }
+
+type StateProvider<'Props, 'State>(props) =
+    inherit Component<ComponentProps<'Props, 'State>, 'State>(props)
+    do base.setInitState props.InitialState
+
+    override this.componentDidMount() =
+        props.Lifecycles
+        |> Option.map (fun l -> l.ComponentDidMount())
+        |> Option.defaultValue()
+
+    member this.update updateFn =
+        this.setState (fun state props -> updateFn state props.Props)
+
+    override this.render() =
+
+        props.Children(this.props.Props, this.state, this.update)
+
+let komponent<'Props, 'State> props initalState lifeCycles (children : OutProps<'Props, 'State> -> ReactElement) =
+    ofType<StateProvider<'Props, 'State>, _, _>
+            {
+                Props = props
+                InitialState = initalState
+                Lifecycles = lifeCycles
+                Children = children
+            }
+            []
