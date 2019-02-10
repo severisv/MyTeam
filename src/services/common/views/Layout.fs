56d9a064475e25d4dd07b1173ca0cc057d0384e4
Severin Sverdvik <severisv@gmail.com>
2019-02-10 11:42:51 +0100

    Oppdater til 2.2

--- src/services/common/views/Layout.fs
+++ src/services/common/views/Layout.fs
@@ -9,27 +9,27 @@ open Microsoft.Extensions.Hosting
 open Giraffe
 
 [<AutoOpen>]
-module Pages =   
+module Pages =
 
     let coachMenuItems = Views.Admin.coachMenuItems
 
 
     let internalMenuItems =
-        [ 
-            li [] [a [_href "/intern" ] [!!(Icons.signup ""); encodedText " Påmelding"]]
-            li [] [a [_href "/intern/oppmote" ] [!!(Icons.attendance ""); encodedText " Oppmøte"]]
-            li [] [a [_href "/intern/boter" ] [!!(Icons.fine ""); encodedText " Bøter"]]
-            li [] [a [_href "/intern/lagliste" ] [!!(Icons.squadList ""); encodedText " Lagliste"]]
+        [
+            li [] [ a [ _href "/intern" ] [ !!(Icons.signup ""); encodedText " Påmelding" ] ]
+            li [] [ a [ _href "/intern/oppmote" ] [ !!(Icons.attendance ""); encodedText " Oppmøte" ] ]
+            li [] [ a [ _href "/intern/boter" ] [ !!(Icons.fine ""); encodedText " Bøter" ] ]
+            li [] [ a [ _href "/intern/lagliste" ] [ !!(Icons.squadList ""); encodedText " Lagliste" ] ]
         ]
-        
+
     type LayoutModel = {
-        Title: string
-        MetaTitle: string
-        MetaDescription: string
-        Scripts: XmlNode list
+        Title : string
+        MetaTitle : string
+        MetaDescription : string
+        Scripts : XmlNode list
     }
- 
-    let layout club (user: Option<Users.User>) getOptions (ctx: HttpContext) content =  
+
+    let layout club (user : Option<Users.User>) getOptions (ctx : HttpContext) content =
 
         let isProduction = ctx.GetService<IHostingEnvironment>().IsProduction()
 
@@ -41,94 +41,94 @@ module Pages =
                             })
 
         let notifications = notifications ctx club
-        let loginPartial = userPartial ctx notifications        
-        let getImage = Images.get ctx                
+        let loginPartial = userPartial ctx notifications
+        let getImage = Images.get ctx
 
-        html [_lang "nb-no"] [
+        html [ _lang "nb-no" ] [
             head [] [
-                meta [_charset "utf-8"]
-                meta [_name "viewport";_content "width=device-width, initial-scale=1.0"]
-                meta [_title <| club.ShortName + (o.Title.HasValue =? (" - " + o.Title, "")) ]
-                o.MetaDescription.HasValue =? (meta [_name "description"; _content o.MetaDescription], emptyText)
-                title [] [encodedText (o.MetaTitle =?? (sprintf "%s - %s" club.Name o.Title)) ]
-                link [_rel "icon"; _type "image/png"; _href <| getImage id (club.Favicon =?? club.Logo) ]
-                link [_rel "apple-touch-icon"; _type "image/png"; _href <| getImage id (club.Favicon =?? club.Logo)]
-                link [_rel "stylesheet"; _href "/compiled/site.bundle.css?v6" ]
+                meta [ _charset "utf-8" ]
+                meta [ _name "viewport"; _content "width=device-width, initial-scale=1.0" ]
+                meta [ _title <| club.ShortName + (o.Title.HasValue =? (" - " + o.Title, "")) ]
+                o.MetaDescription.HasValue =? (meta [ _name "description"; _content o.MetaDescription ], emptyText)
+                title [] [ encodedText (o.MetaTitle =?? (sprintf "%s - %s" club.Name o.Title)) ]
+                link [ _rel "icon"; _type "image/png"; _href <| getImage id (club.Favicon =?? club.Logo) ]
+                link [ _rel "apple-touch-icon"; _type "image/png"; _href <| getImage id (club.Favicon =?? club.Logo) ]
+                link [ _rel "stylesheet"; _href "/compiled/site.bundle.css?v6" ]
                 isProduction =? (Analytics.script, empty)
             ]
-            body []([
-                    div [_class "navbar navbar-inverse navbar-fixed-top"] [
-                        div [_class "container" ] [
-                            div [_class "navbar-header"] [
-                                button [_type "button";_class "navbar-toggle"; attr "data-toggle" "collapse"; attr "data-target" ".navbar-collapse"] [
-                                    span [_class "icon-bar" ] []
-                                    span [_class "icon-bar" ] []
-                                    span [_class "icon-bar" ] []
+            body [] ([
+                    div [ _class "navbar navbar-inverse navbar-fixed-top" ] [
+                        div [ _class "container" ] [
+                            div [ _class "navbar-header" ] [
+                                button [ _type "button"; _class "navbar-toggle"; attr "data-toggle" "collapse"; attr "data-target" ".navbar-collapse" ] [
+                                    span [ _class "icon-bar" ] []
+                                    span [ _class "icon-bar" ] []
+                                    span [ _class "icon-bar" ] []
                                 ]
-                                a [_href "/";_class "navbar-brand" ] [ img [_src <| getImage (fun o -> { o with Width = Some 100 }) club.Logo; _alt club.ShortName ] ]
+                                a [ _href "/"; _class "navbar-brand" ] [ img [ _src <| getImage (fun o -> { o with Width = Some 100 }) club.Logo; _alt club.ShortName ] ]
                             ]
-                            div [_class "navbar-collapse collapse" ] 
+                            div [ _class "navbar-collapse collapse" ]
                                 ([
-                                    ul [_class "nav navbar-nav"] [
-                                        li [] [a [_href "/spillere" ] [ encodedText "Spillere"] ]
-                                        li [] [a [_href "/kamper" ] [ encodedText "Kamper"] ]
-                                        li [] [a [_href "/tabell" ] [ encodedText "Tabell"] ]
-                                        li [] [a [_href "/statistikk" ] [ encodedText "Statistikk"] ]
-                                        li [] [a [_href "/om" ] [ encodedText "Om klubben"] ]
-                                        li [_class "hidden-sm"] [a [_href "/støttespillere" ] [ encodedText "Støttespillere"] ]
-                                        user |> Option.fold(fun _ _ -> 
+                                    ul [ _class "nav navbar-nav" ] [
+                                        li [] [ a [ _href "/spillere" ] [ encodedText "Spillere" ] ]
+                                        li [] [ a [ _href "/kamper" ] [ encodedText "Kamper" ] ]
+                                        li [] [ a [ _href "/tabell" ] [ encodedText "Tabell" ] ]
+                                        li [] [ a [ _href "/statistikk" ] [ encodedText "Statistikk" ] ]
+                                        li [] [ a [ _href "/om" ] [ encodedText "Om klubben" ] ]
+                                        li [ _class "hidden-sm" ] [ a [ _href "/støttespillere" ] [ encodedText "Støttespillere" ] ]
+                                        user |> Option.fold (fun _ _ ->
                                                                 li [] [
-                                                                    a [_class "slide-down-parent hidden-xs"; attr "data-submenu" "#submenu-internal"; _href "javascript:void(0)" ] [
+                                                                    a [ _class "slide-down-parent hidden-xs"; attr "data-submenu" "#submenu-internal"; _href "javascript:void(0)" ] [
                                                                         encodedText "Intern "
                                                                         icon <| fa "angle-down" <| ""
-                                                                    ] 
+                                                                    ]
                                                                 ]
-                                                                                                    
-                                                            ) (emptyText)                     
+
+                                                            ) (emptyText)
                                     ]
                                 ] @ (user |> Option.fold (fun _ user ->
                                             ([
-                                                    hr [_class "visible-xs submenu-divider" ]
-                                                    ul [_class "nav navbar-nav submenu visible-xs"] 
-                                                        internalMenuItems                       
-                                                    
-                                            ] @ (user.IsInRole [Role.Admin;Role.Trener] =?
+                                                    hr [ _class "visible-xs submenu-divider" ]
+                                                    ul [ _class "nav navbar-nav submenu visible-xs" ]
+                                                        internalMenuItems
+
+                                            ] @ (user.IsInRole [ Role.Admin; Role.Trener ] =?
                                                     ([
-                                                        hr [_class "visible-xs submenu-divider"]
-                                                        ul [_class "nav navbar-nav submenu visible-xs adminMenu"] 
+                                                        hr [ _class "visible-xs submenu-divider" ]
+                                                        ul [ _class "nav navbar-nav submenu visible-xs adminMenu" ]
                                                             coachMenuItems
                                                         ], [])))
 
                                             ) []
                             ))
-                            loginPartial user                            
+                            loginPartial user
                             user |> Option.fold (fun _ user ->
-                                                    ul [_id "submenu-internal";_class "nav navbar-nav submenu slide-down-child hidden-xs collapse" ] 
+                                                    ul [ _id "submenu-internal"; _class "nav navbar-nav submenu slide-down-child hidden-xs collapse" ]
                                                         (internalMenuItems @
-                                                         [                                                                             
-                                                            user.IsInRole [Role.Admin;Role.Trener] =?
-                                                                (li [] [a [_href "/admin" ] [!!(Icons.coach ""); encodedText " Admin"]]                       
+                                                         [
+                                                            user.IsInRole [ Role.Admin; Role.Trener ] =?
+                                                                (li [] [ a [ _href "/admin" ] [ !!(Icons.coach ""); encodedText " Admin" ] ]
                                                                 , emptyText)
                                                          ])
 
-                                ) emptyText                               
+                                ) emptyText
                         ]
                     ]
-                    div [_class "mt-page-header"] [
-                        div [_class "container"] [
+                    div [ _class "mt-page-header" ] [
+                        div [ _class "container" ] [
                             h1 [] [
                                 encodedText o.Title
-                            ]                           
+                            ]
                         ]
                     ]
-                    div [_id "main-container";_class "container"] 
+                    div [ _id "main-container"; _class "container" ]
                         content
 
-                    script [_src "/compiled/lib/lib.bundle.js?v22"] []
-                    script [_src "/compiled/app.js?v22" ] []
-                    script [_src "/compiled/client/main.js" ] []
-            ] @ o.Scripts)                
-        ]          
-    
-         
-  
+                    script [ _src "/compiled/lib/lib.bundle.js?v23" ] []
+                    script [ _src "/compiled/app.js?v23" ] []
+                    script [ _src "/compiled/client/main.js?v1" ] []
+            ] @ o.Scripts)
+        ]
+
+
+
