dc65a2eb3fe21312566f7b4431ec92f21c2c5e06
Severin Sverdvik <severisv@gmail.com>
2020-01-29 07:45:45 +0100

    Update Startup.cs

--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -78,11 +78,11 @@ namespace MyTeam
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.LogStart();
 
-            if (env.IsDevelopment() || env.IsStaging())
+            if (env.EnvironmentName == "Development"|| env.EnvironmentName == "staging")
             {
                 app.UseDatabaseErrorPage();
             }
