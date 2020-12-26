8880c633af5d49a674754f9a9692b8a24edc1a1d
Severin Sverdvik <severisv@gmail.com>
2020-12-26 07:46:59 +0100

    Fixes #277

--- src/client/src/features/players/PlayerStats.fs
+++ src/client/src/features/players/PlayerStats.fs
@@ -68,63 +68,66 @@ let element =
                       OnError = printf "%O" }),
              [| props.PlayerId |])
 
-        div
-            [ Class "playerStats" ]
-            ([h4 [] [str "Stats"]] @
-            (stats.current
-             |> List.mapi (fun i year ->
-                 let isCollapsed =
-                     (i = 0 || (i = (stats.current.Length - 1)))
-                     |> function
-                     | true -> Open
-                     | _ -> Collapsed
-                     
-
-                 Collapsible.collapsible
-                     isCollapsed
-                     [ div [ Class "playerStats-year" ] [
-                         (if year.Year = 0 then "Totalt" else string year.Year)
-                         |> str
-                       ] ]
-                     [ table [ Class "table playerStats" ] [
-                         tbody
-                             []
-                             (year.Teams
-                              |> List.map (fun season ->
-                                  tr [] [
-                                      td [] [ str season.TeamName ]
-                                      td [ Class "playerStats--value" ] [
-                                          Icons.player "Kamper"
-                                          str <| string season.Games
-                                      ]
-                                      td [ Class "playerStats--value" ] [
-                                          Icons.goal "Mål"
-                                          str <| string season.Goals
-                                      ]
-                                      td [ Class "playerStats--value" ] [
-                                          Icons.assist "Assists"
-                                          str <| string season.Assists
-                                      ]
-                                      td
-                                          [ Class "playerStats--value kort" ]
-                                          (if season.YellowCards > 0 then
-                                              [ Icons.yellowCard "Gule kort"
-                                                str <| string season.YellowCards ]
-                                           else
-                                               [])
-                                      td
-                                          [ Class "playerStats--value kort" ]
-                                          (if season.RedCards > 0 then
-                                              [ Icons.redCard "Røde kort"
-                                                str <| string season.RedCards ]
-                                           else
-                                               [])
-                                  ]))
-                       ] ])))
+        if stats.current
+           |> List.filter (fun s -> not s.Teams.IsEmpty)
+           |> List.isEmpty
+           |> not then
+            div
+                [ Class "playerStats" ]
+                ([ h4 [] [ str "Stats" ] ]
+                 @ (stats.current
+                    |> List.mapi (fun i year ->
+                        let isCollapsed =
+                            (i = 0 || (i = (stats.current.Length - 1)))
+                            |> function
+                            | true -> Open
+                            | _ -> Collapsed
 
 
+                        Collapsible.collapsible
+                            isCollapsed
+                            [ div [ Class "playerStats-year" ] [
+                                (if year.Year = 0 then "Totalt" else string year.Year)
+                                |> str
+                              ] ]
+                            [ table [ Class "table playerStats" ] [
+                                tbody
+                                    []
+                                    (year.Teams
+                                     |> List.map (fun season ->
+                                         tr [] [
+                                             td [] [ str season.TeamName ]
+                                             td [ Class "playerStats--value" ] [
+                                                 Icons.player "Kamper"
+                                                 str <| string season.Games
+                                             ]
+                                             td [ Class "playerStats--value" ] [
+                                                 Icons.goal "Mål"
+                                                 str <| string season.Goals
+                                             ]
+                                             td [ Class "playerStats--value" ] [
+                                                 Icons.assist "Assists"
+                                                 str <| string season.Assists
+                                             ]
+                                             td
+                                                 [ Class "playerStats--value kort" ]
+                                                 (if season.YellowCards > 0 then
+                                                     [ Icons.yellowCard "Gule kort"
+                                                       str <| string season.YellowCards ]
+                                                  else
+                                                      [])
+                                             td
+                                                 [ Class "playerStats--value kort" ]
+                                                 (if season.RedCards > 0 then
+                                                     [ Icons.redCard "Røde kort"
+                                                       str <| string season.RedCards ]
+                                                  else
+                                                      [])
+                                         ]))
+                              ] ])))
 
-        )
+        else
+            fr [] [])
 
 printf "hydrating %O" containerId
 hydrate2 containerId Decode.Auto.fromString<Props> element
