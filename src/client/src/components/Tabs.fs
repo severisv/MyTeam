39e4ca9a6ca700db73d48a849b9bbe4bf846fb38
Severin Sverdvik <severisv@gmail.com>
2021-11-21 09:43:01 +0100

    Legg til femto

--- src/client/src/components/Tabs.fs
+++ src/client/src/components/Tabs.fs
@@ -6,25 +6,43 @@ open Fable.React
 open Fable.React
 open Fable.React.Props
 
-type Tab = {
-    Text: string
-    ShortText: string
-    Url: string
-    Icon: ReactElement option
-}
+type Tab =
+    { Text: string
+      ShortText: string
+      Url: string
+      Icon: ReactElement option }
 
 let tabs attributes (items: Tab list) (isSelected: (string -> bool)) =
     if items.Length > 1 then
-        div attributes [
-            ul [Class "nav nav-pills"] 
-                (items 
-                 |> List.map (fun t -> 
-                                li [Class (if isSelected t.Url then "active" else "")] [
-                                    a [Href t.Url] [
-                                        span [Class "hidden-xs" ] [t.Icon |> Option.defaultValue empty] 
-                                        span [Class "hidden-xs"] [whitespace;str t.Text] 
-                                        span [Class "visible-xs"] [str t.ShortText]
-                                    ]
-                                ]))
-            ]
-    else empty         
+        div
+            attributes
+            [ ul
+                  [ Class "nav nav-pills" ]
+                  (items
+                   |> List.map
+
+
+
+
+                       (fun t ->
+                           li [ Class(
+                                    if isSelected t.Url then
+                                        "active"
+                                    else
+                                        ""
+                                ) ] [
+                               a [ Href t.Url ] [
+                                   span [ Class "hidden-xs" ] [
+                                       t.Icon |> Option.defaultValue empty
+                                   ]
+                                   span [ Class "hidden-xs" ] [
+                                       whitespace
+                                       str t.Text
+                                   ]
+                                   span [ Class "visible-xs" ] [
+                                       str t.ShortText
+                                   ]
+                               ]
+                           ])) ]
+    else
+        empty
