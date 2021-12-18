e11ad98a5992a98a1a1811f9bd4550216f0948ee
Severin Sverdvik <severisv@gmail.com>
2021-12-18 13:15:51 +0100

    Update Json.fs

--- src/server/utils/Json.fs
+++ src/server/utils/Json.fs
@@ -7,15 +7,12 @@ open Thoth.Json.Net
 module Json =
     let serialize obj =
         let settings = JsonSerializerSettings()
-        settings.ContractResolver <- Serialization.CamelCasePropertyNamesContractResolver
-                                         ()
+        settings.ContractResolver <- Serialization.CamelCasePropertyNamesContractResolver()
         settings.Converters.Add(OptionConverter())
         settings.Converters.Add(IdiomaticDuConverter())
         settings.Converters.Add(StringEnumConverter())
         JsonConvert.SerializeObject(obj, settings)
-    
-    let fableSerialize obj = 
-        Encode.Auto.toString (0, obj)
 
-    let fableDeserialize<'t> = 
-        Decode.Auto.fromString<'t>    
+    let fableSerialize obj = Encode.Auto.toString (0, obj)
+
+    let fableDeserialize<'t> = Decode.Auto.fromString<'t>
