bf3f975271bb0ae68abb94bd4145ee422d60f5f2
Severin Sverdvik <severisv@gmail.com>
2022-01-08 10:50:34 +0100

    Log body

--- src/server/features/account/RequestDeletion.fs
+++ src/server/features/account/RequestDeletion.fs
@@ -10,36 +10,44 @@ open Giraffe.ViewEngine.HtmlElements
 open Giraffe.ViewEngine.Attributes
 open Shared.Domain
 open Giraffe.Core
+open System.IO
 
-let requestDeletion next (ctx: HttpContext)  =
+let requestDeletion: HttpHandler = 
+    fun next (ctx: HttpContext) ->
 
-    let userId =
-        try
-            let signed_request =
-                ctx.Request.Form["signed_request"] |> string
+            let userId =
+                try
+                    let signed_request =
+                        ctx.Request.Form["signed_request"] |> string
 
-            let split = signed_request.Split('.')
-            let dataRaw = split[1]
+                    let split = signed_request.Split('.')
+                    let dataRaw = split[1]
 
-            let dataBuffer = Convert.FromBase64String(dataRaw)
-            let json = Encoding.UTF8.GetString(dataBuffer)
+                    let dataBuffer = Convert.FromBase64String(dataRaw)
+                    let json = Encoding.UTF8.GetString(dataBuffer)
 
-            let fbUser =
-                JsonConvert.DeserializeObject<{| user_id: string |}>(json)
+                    let fbUser =
+                        JsonConvert.DeserializeObject<{| user_id: string |}>(json)
 
-            fbUser.user_id
-        with
-        | e -> e.Message.Substring(0, 22)
+                    fbUser.user_id
+                with
+                | e -> e.Message.Substring(0, 22)
 
 
-    let logger = Logger.get ctx.RequestServices
+            let logger = Logger.get ctx.RequestServices
 
-    logger.LogInformation(EventId(3), $"User {userId} requested deletion.")
 
-    json {| url = $"{ctx.Request.Scheme}://{ctx.Request.Host}/konto/sletting/{userId}"
-            confirmation_code = userId |}
-            next ctx
+            let body = new StreamReader(ctx.Request.Body)
+            let body = body.ReadToEndAsync().Result
+            
+            logger.LogInformation(EventId(3), $"User {userId} requested deletion.")
+            logger.LogInformation(EventId(3), body )
 
+            json {| url = $"{ctx.Request.Scheme}://{ctx.Request.Host}/konto/sletting/{userId}"
+                    confirmation_code = userId |}
+                            next ctx
+
+    
         
 
 let showStatus (club: Club) user userId (ctx: HttpContext) =
