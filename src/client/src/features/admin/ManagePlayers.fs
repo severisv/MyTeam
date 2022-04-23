69b03fd909eabc022a25fe4c048f824586702bac
Severin Sverdvik <severisv@gmail.com>
2022-04-23 10:53:01 +0200

    wip

--- src/client/src/features/admin/ManagePlayers.fs
+++ src/client/src/features/admin/ManagePlayers.fs
@@ -35,113 +35,126 @@ let ManagePlayers (props: Props) =
     |> List.sortBy (fun (key, _) -> key)
     |> List.map (fun (key, members) ->
         Html.div [
-            Html.h2 [
-                prop.style [ style.textAlign.left ]
-                prop.children [
-                    Html.text (string key)
-                    Html.span [
-                        prop.style [
-                            style.fontSize (length.px 22)
+            prop.style [ style.overflowX.auto ]
+            prop.children [
+
+                Html.h2 [
+                    prop.style [ style.textAlign.left ]
+                    prop.children [
+                        Html.text (string key)
+                        Html.span [
+                            prop.style [
+                                style.fontSize (length.px 22)
+                            ]
+                            prop.children [
+                                Html.text $" ({members.Length})"
+                            ]
                         ]
-                        prop.children [
-                            Html.text $" ({members.Length})"
+
                         ]
-                    ]
-
-                    ]
-            ]
-            table
-                [ Striped
-                  (Attribute(
-                      Fable.React.Props.Style [
-                          Fable.React.Props.CSSProp.MarginBottom "40px"
-                      ]
-                  )) ]
-                [ col [ CellType Image
-                        NoSort
-                        Visible(ScreenLg) ] []
-                  col [ NoSort ] [ Html.text "Navn" ]
-                  col [ NoSort ] [ Html.text "Lag" ]
-                  col [ NoSort ] [ Html.text "Roller" ]
-                  col [ NoSort ] [ Html.text "Status" ]
-                  col [ NoSort ] [] ]
-                (members
-                 |> List.map (fun m ->
-                     tableRow [] [
-                         Html.img [
-                             prop.src
-                             <| Image.getMember
-                                 props.ImageOptions
-                                 (fun o ->
-                                     { o with
-                                         Height = Some 40
-                                         Width = Some 40 })
-                                 m.Details.Image
-                                 m.Details.FacebookId
-                         ]
-
-                         Html.text m.Details.FullName
-
-                         Html.div [
-                             prop.style [
-                                 style.display.flex
-                                 style.gap (length.px 2, length.px 2)
+                ]
+                table
+                    [ Striped
+                      (Attribute(
+                          Fable.React.Props.Style [
+                              Fable.React.Props.CSSProp.MarginBottom "40px"
+                          ]
+                      )) ]
+                    [ col [ CellType Image
+                            NoSort
+                            Visible(ScreenLg) ] []
+                      col [ NoSort ] [ Html.text "Navn" ]
+                      col [ NoSort ] [ Html.text "Lag" ]
+                      col [ NoSort ] [ Html.text "Roller" ]
+                      col [ NoSort ] [ Html.text "Status" ]
+                      col [ NoSort ] [] ]
+                    (members
+                     |> List.map (fun m ->
+                         tableRow [] [
+                             Html.img [
+                                 prop.src
+                                 <| Image.getMember
+                                     props.ImageOptions
+                                     (fun o ->
+                                         { o with
+                                             Height = Some 40
+                                             Width = Some 40 })
+                                     m.Details.Image
+                                     m.Details.FacebookId
                              ]
-                             prop.children (
-                                 props.Teams
-                                 |> List.map (fun t ->
-                                     let team =
-                                         m.Teams |> List.tryFind (fun team -> team = t.Id)
 
-                                     btn [ (team
-                                            |> Option.map (fun _ -> Primary)
-                                            |> Option.defaultValue Default)
-                                           Sm
-                                           Fable.React.Props.OnClick(fun _ -> ()) ] [
-                                         Html.text (t.ShortName)
-                                     ]
+                             Html.text m.Details.FullName
 
+                             Html.div [
+                                 prop.style [
+                                     style.display.flex
+                                     style.gap (length.px 2, length.px 2)
+                                 ]
+                                 prop.children (
+                                     props.Teams
+                                     |> List.map (fun t ->
+                                         let team =
+                                             m.Teams |> List.tryFind (fun team -> team = t.Id)
+
+                                         btn [ (team
+                                                |> Option.map (fun _ -> Primary)
+                                                |> Option.defaultValue Default)
+                                               Sm
+                                               Fable.React.Props.OnClick(fun _ -> ()) ] [
+                                             Html.text (t.ShortName)
+                                         ]
+
+                                     )
                                  )
-                             )
-                         ]
-
-
-                         Html.div [
-                             prop.style [
-                                 style.display.flex
-                                 style.gap (length.px 2, length.px 2)
                              ]
-                             prop.children (
-                                 (m.Roles
-                                  |> List.map (fun role ->
-                                      label
-                                          LightBlue
-                                          [ Html.text (string role)
-                                            Icons.remove ]))
-                                 @ [ Html.button [
-                                         prop.style [
-                                             style.marginLeft (length.px 2)
-                                             style.fontSize (length.px 10)
-                                         ]
-                                         prop.children [
-                                             Icons.add "Tildel ny rolle"
-                                         ]
-                                     ] ]
-                             )
-                         ]
-                         btn [ Default
-                               Sm
-                               Fable.React.Props.Style [
-                                   Fable.React.Props.CSSProp.MarginRight "2px"
-                               ]
-                               Fable.React.Props.OnClick(fun _ -> ()) ] [
-                             Icons.chevronDown
-                         ]
-                         Html.a [ Icons.edit "Rediger profil" ]
-                     ]))
 
 
-            ]
+                             Html.div [
+                                 prop.style [
+                                     style.display.flex
+                                     style.flexWrap.wrap
+                                     style.gap (length.px 2, length.px 2)
+                                 ]
+                                 prop.children (
+                                     (m.Roles
+                                      |> List.map (fun role ->
+                                          label
+                                              LightBlue
+                                              [ Html.text (string role)
+                                                Html.text " "
+                                                Icons.remove ]))
+                                     @ [ Html.button [
+                                             prop.style [
+                                                 style.marginLeft (length.px 2)
+                                                 style.fontSize (length.px 10)
+                                                 style.minHeight (length.px 26)
+                                                 style.minWidth (length.px 26)
+                                             ]
+                                             prop.children [
+                                                 Icons.add "Tildel ny rolle"
+                                             ]
+                                         ] ]
+                                 )
+                             ]
+                             btn [ Default
+                                   Sm
+                                   Fable.React.Props.Style [
+                                       Fable.React.Props.CSSProp.MarginRight "2px"
+                                   ]
+                                   Fable.React.Props.OnClick(fun _ -> ()) ] [
+                                 Icons.chevronDown
+                             ]
+                             Html.a [
+                                 prop.href $"/spillere/endre/{m.Details.UrlName}"
+                                 prop.children [
+                                     Icons.edit "Rediger profil"
+                                 ]
+                             ]
+                         ]))
+
+
+                ]
+        ]
 
     )
     |> Html.div
