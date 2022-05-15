bff25a29bee595c35b5beb15ed312ed7f3182c5e
Severin Sverdvik <severisv@gmail.com>
2022-05-15 17:17:26 +0200

    Update AntiForgery.fs

--- src/server/utils/AntiForgery.fs
+++ src/server/utils/AntiForgery.fs
@@ -4,19 +4,30 @@ open Microsoft.AspNetCore.Antiforgery
 open Giraffe
 
 
-let getToken (ctx : HttpContext) =
+let getToken (ctx: HttpContext) =
     let antiforgery = ctx.GetService<IAntiforgery>()
     antiforgery.GetAndStoreTokens(ctx).RequestToken
 
 
 let validate =
-    fun (next : HttpFunc) (ctx : HttpContext) ->
+    fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
             let antiforgery = ctx.GetService<IAntiforgery>()
-                
+
             let! isValid = antiforgery.IsRequestValidAsync ctx
 
             if isValid then
                 return! next ctx
-            else return! failwith "Invalid antiforgery token"    
+
+
+            else
+                let clientDetails =
+                    $"""Referer: {(string ctx.Request.Headers.["Referer"])} \n{(ctx.Request.Headers
+                                                                                |> Seq.filter (fun kv -> not <| kv.Key.ToLower().Contains "cookie")
+                                                                                |> Seq.map (fun keyValue -> $"%s{keyValue.Key}: %s{string keyValue.Value}")
+                                                                                |> String.concat ",\n  ")}"""
+
+
+
+                return! failwithf "Invalid antiforgery token\n%O" clientDetails
         }
