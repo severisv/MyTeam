876838079e31964901b59aa10aad91060cc0bf06
Severin Sverdvik <severisv@gmail.com>
2022-05-22 12:24:15 +0200

    Update app.js

--- src/server/client/scripts/app.js
+++ src/server/client/scripts/app.js
@@ -3,10 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './lib/jquery.tablesorter.min'
 import '../stylesheets/style.less'
 
-import ajax from './ajaxHelpers'
 import global from './global'
-
-window.ajax = ajax
 window.checkbox = require('./checkbox')
-
 window.global = global
