8da441a016df790d18d66c3ab1baf3d61d751867
Severin Sverdvik <severisv@gmail.com>
2024-07-07 10:51:32 +0200

    Update packages

--- src/server/features/news/Persistence.fs
+++ src/server/features/news/Persistence.fs
@@ -7,78 +7,84 @@ open MyTeam
 open System
 open System.Linq
 
-let saveArticle : UpdateArticle =
+let saveArticle: UpdateArticle =
     fun db clubId articleName model ->
         let (ClubId clubId) = clubId
 
-        db.Articles.Where (fun a -> a.ClubId = clubId && a.Name = articleName)
+        db.Articles.Where(fun a -> a.ClubId = clubId && a.Name = articleName)
         |> Seq.tryHead
         |> function
-        | Some a -> 
-              a.Headline <- model.Headline
-              a.Content <- model.Content
-              a.ImageUrl <- model.ImageUrl
-              a.HideAuthor <- model.HideAuthor
-              a.GameId <- model.IsMatchReport =? (model.GameId |> toNullable, Nullable())           
-              db.SaveChanges() |> ignore
-              OkResult()
+            | Some a ->
+                a.Headline <- model.Headline
+                a.Content <- model.Content
+                a.ImageUrl <- model.ImageUrl
+                a.HideAuthor <- model.HideAuthor
 
-        | None -> NotFound                                                  
+                a.GameId <-
+                    model.IsMatchReport
+                    =? (model.GameId |> toNullable, Nullable())
+
+                db.SaveChanges() |> ignore
+                OkResult()
+
+            | None -> NotFound
 
 
-let createArticle : CreateArticle =
+let createArticle: CreateArticle =
     fun db clubId user model ->
         let (ClubId clubId) = clubId
 
-        let articleName = 
+        let articleName =
 
             let rgx = Text.RegularExpressions.Regex("[^a-zA-Z0-9 -]")
 
             let rec appendNumberIfTaken name =
                 let isTaken n =
                     query {
-                            for a in db.Articles do
+                        for a in db.Articles do
                             exists (a.Name = n)
                     }
 
                 if isTaken name then
                     appendNumberIfTaken (name + "-1")
-                else name                
+                else
+                    name
 
-            model.Headline.Replace(" ", "-") 
+            model.Headline.Replace(" ", "-")
             |> toLower
             |> fun value -> rgx.Replace(value, "")
-            |> appendNumberIfTaken                
+            |> appendNumberIfTaken
 
         let article = MyTeam.Models.Domain.Article()
         article.Headline <- model.Headline
         article.Content <- model.Content
         article.ImageUrl <- model.ImageUrl
-        article.GameId <- model.IsMatchReport =? (model.GameId |> toNullable, Nullable())           
+
+        article.GameId <-
+            model.IsMatchReport
+            =? (model.GameId |> toNullable, Nullable())
+
         article.AuthorId <- Nullable(user.Id)
         article.Published <- DateTime.Now
         article.ClubId <- clubId
         article.Name <- articleName
-              
-        db.Articles.Add(article) |> ignore      
+        article.HideAuthor <- model.HideAuthor
+
+        db.Articles.Add(article) |> ignore
         db.SaveChanges() |> ignore
         OkResult articleName
 
 
-let deleteArticle : DeleteArticle =
+let deleteArticle: DeleteArticle =
     fun db clubId articleName ->
         let (ClubId clubId) = clubId
 
         db.Articles.Where(fun a -> a.ClubId = clubId && a.Name = articleName)
-        |> Seq.tryHead 
+        |> Seq.tryHead
         |> function
-        | Some a -> 
-              db.Remove(a) |> ignore    
-              db.SaveChanges() |> ignore
-              OkResult()
+            | Some a ->
+                db.Remove(a) |> ignore
+                db.SaveChanges() |> ignore
+                OkResult()
 
-        | None -> NotFound                                                  
-
-
-           
-
+            | None -> NotFound
