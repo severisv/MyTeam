65c88a0b7a212c669f0225e036650f8bbe1e380b
Severin Sverdvik <severisv@gmail.com>
2024-09-12 14:47:48 +0200

    Repair refresh tables

--- src/server/features/table/Refresh.fs
+++ src/server/features/table/Refresh.fs
@@ -8,7 +8,7 @@ open FSharp.Data
 open Microsoft.Extensions.Logging
 
 #if !DEBUG
-type TableHtml = HtmlProvider<"https://www.fotball.no/fotballdata/turnering/tabell/?fiksId=158443">
+type TableHtml = HtmlProvider<"https://www.fotball.no/fotballdata/turnering/tabell/?fiksId=192858">
 #else
 type TableHtml = HtmlProvider<"features/table/table.html">
 #endif
@@ -20,51 +20,71 @@ let run next (ctx: HttpContext) =
 
     query {
         for season in db.Seasons do
-            where
-                (season.StartDate
-                 <= now
-                 && season.EndDate >= now
-                 && season.AutoUpdateTable
-                 && season.TableSourceUrl <> null)
+            where (
+                season.StartDate <= now
+                && season.EndDate >= now
+                && season.AutoUpdateTable
+                && season.TableSourceUrl <> null
+            )
+
             select (season)
     }
     |> Seq.toList
     |> List.iter (fun season ->
-        try
-            let html = TableHtml.Load(season.TableSourceUrl)
+        //try
+        let html = TableHtml.Load(season.TableSourceUrl)
 
-            let table =
-                html.Tables.Table1.Rows
-                |> Array.filter (fun row -> row.Plass |> Number.isNumber)
-                |> Array.map (fun row ->
-                    { Team = row.Lag
-                      Position = row.Plass |> int
-                      Points = row.Poeng
-                      GoalsFor =
-                          row.``Total - Mål``
-                          |> Strings.split '-'
-                          |> List.head
-                          |> Strings.trim
-                          |> int
-                      GoalsAgainst =
-                          row.``Total - Mål``
-                          |> Strings.split '-'
-                          |> List.last
-                          |> Strings.trim
-                          |> int
-                      Wins = row.``Total - V``
-                      Draws = row.``Total - U``
-                      Losses = row.``Total - T`` }
+        let table = html.Html.CssSelect("table") |> Seq.head
 
-                    )
+        let indices =
+            {| Plass = 0
+               Lag = 1
+               Poeng = 16
+               Mål = 14
+               V = 11
+               U = 12
+               T = 13 |}
 
-            season.TableJson <- Json.fableSerialize table
-            season.TableUpdated <- now
-            db.SaveChanges() |> ignore
-        with ex ->
-            logger.LogError
-                (EventId(),
-                 ex,
-                 sprintf "Klarte ikke scrape tabell. SeasonId: %O  Url: %s" season.Id season.TableSourceUrl))
+        let table =
+            table.CssSelect("tr")
+            |> List.skip 2
+            |> List.map (fun row ->
+
+                ({| Plass = (row.CssSelect("td").[indices.Plass]).InnerText()
+                    Lag = (row.CssSelect("td").[indices.Lag]).InnerText()
+                    Poeng = (row.CssSelect("td").[indices.Poeng]).InnerText()
+                    Mål = (row.CssSelect("td").[indices.Mål]).InnerText()
+                    V = (row.CssSelect("td").[indices.V]).InnerText()
+                    U = (row.CssSelect("td").[indices.U]).InnerText()
+                    T = (row.CssSelect("td").[indices.T]).InnerText() |}))
+            |> List.filter (fun row -> row.Plass |> Number.isNumber)
+            |> List.map (fun row ->
+                { Team = row.Lag
+                  Position = row.Plass |> int
+                  Points = row.Poeng |> int
+                  GoalsFor =
+                    row.Mål
+                    |> Strings.split '-'
+                    |> List.head
+                    |> Strings.trim
+                    |> int
+                  GoalsAgainst =
+                    row.Mål
+                    |> Strings.split '-'
+                    |> List.last
+                    |> Strings.trim
+                    |> int
+                  Wins = row.V |> int
+                  Draws = row.U |> int
+                  Losses = row.T |> int }
+
+            )
+
+        season.TableJson <- Json.fableSerialize table
+        season.TableUpdated <- now
+        db.SaveChanges() |> ignore
+    // with
+    // | ex -> logger.LogError(EventId(), ex, sprintf "Klarte ikke scrape tabell. SeasonId: %O  Url: %s" season.Id season.TableSourceUrl)
+    )
 
     text "Ok" next ctx
